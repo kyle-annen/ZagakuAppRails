b2fb8e31571fea2f472c36c89009f7fe1c987c98
Kyle Annen <kyleannen@gmail.com>
2017-11-09 13:30:21 -0600

    omniauth login half working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'static_pages#index'
   
   get '/redirect', to: 'google_calendar#redirect', as: 'redirect'
   get '/callback', to: 'google_calendar#callback', as: 'callback'
   get '/calendar', to: 'googel_calendar#show', as: 'calendar'
+  
 
   
 end
