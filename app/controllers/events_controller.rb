c6be33a76f5491a846fd23be83812d884ee40502
Kyle Annen <kyleannen@gmail.com>
2017-11-16 14:25:27 -0600

    Finish prototype table layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,6 @@ class EventsController < ApplicationController
 
   def index
     GoogleCalendarService.sync_calendar
-    @events = Event.where('start_time > ?', DateTime.now).order('start_time asc')
+    @events = Event.all
   end
 end
