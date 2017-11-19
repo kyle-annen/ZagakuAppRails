15ca3226c4e7f42264a96a2e3df0ae19c2a98d3c
Kyle Annen <kyleannen@gmail.com>
2017-11-19 12:29:40 -0600

    Finish GithubService

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,10 +1,32 @@
 
 module GithubService
-  def get_directory_contents(repository, repository_path)
-    client = Octokit::Client.new \
-      :client_id => ENV['GITHUB_CLIENT_ID'],
-      :client_secret => ENV['GITHUB_CLIENT_SECRET']
-    user = client.user 'kyle-annen'
-    Octokit.contents(repository, :path => repository_path)
+  @user = 'kyle-annen'
+
+  def get_files(repo, start_path)
+    get_contents(repo, start_path)
+        .flat_map { |o| resolve_contents(o, repo) }
   end
+
+
+
+  def get_contents(repository, repository_path)
+    client = Octokit::Client.new(
+        :client_id => ENV['GITHUB_CLIENT_ID'],
+        :client_secret => ENV['GITHUB_CLIENT_SECRET'])
+    user = client.user @user
+    client.contents(repository, :path => repository_path)
+  end
+
+  def files_resolved?(repo_listing_array)
+    results = []
+    repo_listing_array.each do |listing|
+      results << (listing.type == 'file')
+    end
+    results.inject(true) { |x, y| x && y }
+  end
+
+  private
+    def resolve_contents(object, repo)
+      object.type == 'dir' ? get_contents(repo, object.path) : object
+    end
 end
