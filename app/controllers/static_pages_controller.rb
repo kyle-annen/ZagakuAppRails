21ff3fc980f008d0ea930f26799f2386f9e60408
scottyplunkett <splunkett@8thLight.com>
2017-12-14 19:47:01 -0600

    StaticPagesController interface segregated.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,21 @@
 class StaticPagesController < ApplicationController
+  include StaticPagesHelper
   def index
-    @team_photos = MetaInspector.new('https://8thlight.com/team/').images
+    @this_week = setup_week
   end
+
+  private
+
+  def setup_week
+    StaticPagesHelper.get_weeks_event_details_and_photos(upcoming_events,team_photos)
+  end
+
+  def upcoming_events
+    Event.where(start_time: Time.now.beginning_of_week..Time.now.end_of_week)
+  end
+
+  def team_photos
+    MetaInspector.new('https://8thlight.com/team/').images.to_a
+  end
+
 end
